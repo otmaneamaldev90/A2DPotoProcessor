abc80b2eaa53b4c60036cad408e09621ce0721dc
Othmane AMAL <otmane.a@v12software.com>
2024-10-26 00:27:00 +0100

    test

--- src/Services/CloudinaryProcessing.php
+++ src/Services/CloudinaryProcessing.php
@@ -19,13 +19,7 @@ class CloudinaryProcessing
     {
         $results = [];
         $photos = $this->params['photos'] ?? [];
-        $width = $this->params['width'] ?? 800;
-        $height = $this->params['height'] ?? 600;
         $quality = $this->params['quality'] ?? 100;
-        $fill = $this->params['fill'] ?? 1;
-        $overlay_images = $this->params['overlay_images'] ?? [];
-        $watermark = $this->params['watermark_images'] ?? '';
-        $brightness = $this->params['brightness'] ?? null;
         $contrast = $this->params['contrast'] ?? null;
 
         $cloudinary = new Cloudinary([
@@ -45,59 +39,25 @@ class CloudinaryProcessing
 
             foreach ($photos as $key => $photo) {
                 $transformations = [];
-
-                if ($fill == 1) {
-                    if (!empty($this->params['default_bg_color'])) {
-                        $hex = ltrim($this->params['default_bg_color'], '#');
-                        $transformations[] = ['effect' => 'fill:rgb:' . $hex];
-                    } elseif (!empty($this->params['default_bg_color_blur'])) {
-                        $transformations[] = ['effect' => 'blur'];
-                    } else {
-                        $transformations[] = ['crop' => 'fill'];
-                    }
-                }
-
-                if ($brightness !== null && is_numeric($brightness)) {
-                    $transformations[] = ['effect' => 'brightness:' . $brightness];
-                }
+                $transformations[] = ['quality' => $quality];
 
                 if ($contrast !== null && is_numeric($contrast)) {
                     $transformations[] = ['effect' => 'contrast:' . $contrast];
                 }
 
-                $transformations[] = ['quality' => $quality];
-                $transformations[] = ['width' => $width, 'height' => $height];
-
                 $photo_url_origin = "{$this->params['user_id']}/{$this->vehicle_id}/{$photo['photo']}";
 
-                if ($this->shouldApplyWatermark($overlay_images, $key, $photos, $watermark)) {
-                    $transformations[] = ['overlay' => $watermark];
+                if (isset($photo_url_origin) && !empty($photo_url_origin)) {
+                    $result = $cloudinary->uploadApi()->upload($photo_url_origin, ['transformation' => $transformations]);
+                    $results[] = $result['secure_url'];
+                } else {
+                    \Log::warning("Photo URL origin is null or empty");
                 }
-
-                $result = $cloudinary->uploadApi()->upload($photo_url_origin, ['transformation' => $transformations]);
-                $results[] = $result->getSecureUrl();
             }
         } else {
             \Log::warning("No photos were found");
         }
 
         return $results;
-    }
-
-    private function shouldApplyWatermark($overlay_images, $key, $photos, $watermark)
-    {
-        if (in_array('1', $overlay_images) && $key == 0 && !empty($watermark)) {
-            return true;
-        }
-
-        if (in_array('2', $overlay_images) && $key != 0 && $key != (count($photos) - 1) && !empty($watermark)) {
-            return true;
-        }
-
-        if (in_array('3', $overlay_images) && $key == (count($photos) - 1) && !empty($watermark)) {
-            return true;
-        }
-
-        return false;
     }
 }
